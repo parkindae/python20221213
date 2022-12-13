829c1ff71faa1b6ebabc51c4ba78bc64b5c0b874
parkindae <pa827@naver.com>
2022-12-13 13:57:57 +0900

    함수연습2

--- function2.py
+++ function2.py
@@ -16,3 +16,33 @@ def swap(x,y):
 #호출
 print( swap(3,4) )
 
+print("---불변형식---")
+a = 1.2
+print( id(a) )
+a = 2.3
+print( id(a) )
+
+print("---가변형식---")
+lst = [1,2,3]
+print( id(lst) )
+lst.append(4)
+print( id(lst) )
+
+print("---이름을 해석하는 순서---")
+#함수정의(LGB)
+def func1(a):
+    x = 1 
+    return x+a 
+
+#호출
+print( func1(1) )
+
+#함수정의
+x = 5 
+def func2(a):
+    return x+a 
+
+#호출
+print( func2(1) )
+
+
