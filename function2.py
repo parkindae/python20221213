864bbd9cdf35eed37e52e26217dca458f2c99915
parkindae <pa827@naver.com>
2022-12-13 13:52:18 +0900

    함수연습3

--- function2.py
+++ function2.py
@@ -1,16 +1,17 @@
-# function2.py
+# function2.py 
 def setValue(newValue):
-    #지역변수
+    #지역변수 
     x = newValue
     print(x)
 
 #호출
+#디버깅할 때 중단점
 retValue = setValue(5)
 print(retValue)
 
-#함수정의
+#함수 정의
 def swap(x,y):
-    return y,x
+    return y,x 
 
 #호출
 print( swap(3,4) )
