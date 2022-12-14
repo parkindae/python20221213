2a3789e68e5eadefe1c5617d2ec8e17bad70cf55
parkindae <pa827@naver.com>
2022-12-14 16:29:23 +0900

    에러처리2

--- # try1.py
+++ # try1.py
@@ -1,12 +1,21 @@
 # try1.ty
 # 함수정의
-def dicide(a,b):
+def divide(a,b):
     return a/b
 
-#함수호출
-result = divide(5,0)
-print("결과:{0}".format(result))
-
+#에러처리
+try:
+    #함수호출
+    result = divide(5,0)
+    
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다.")
+except TypeError:
+    print("숫자여야 연산이 됩니다.")
+else:    
+    print("결과:{0}".format(result))
+finally:
+    print("한번 더 체크(이중체크)")
 
 print("전체 코드 실행 종료")
 
