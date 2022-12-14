21011172b64ad2df29af758b8944402b91773b2f
parkindae <pa827@naver.com>
2022-12-14 16:34:12 +0900

    에러처리2

--- # try1.py
+++ # try1.py
@@ -1,22 +1,20 @@
-# try1.ty
-# 함수정의
+# try1.py 
+#함수 정의
 def divide(a,b):
-    return a/b
+    return a/b 
 
 #에러처리
 try:
     #함수호출
-    result = divide(5,0)
-    
+    result = divide(5,2)
 except ZeroDivisionError:
     print("0으로 나누면 안됩니다.")
 except TypeError:
     print("숫자여야 연산이 됩니다.")
-else:    
+else:
     print("결과:{0}".format(result))
 finally:
     print("한번 더 체크(이중체크)")
 
+
 print("전체 코드 실행 종료")
-
-
